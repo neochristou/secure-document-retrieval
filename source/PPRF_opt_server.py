4bbd4307d14bf8529cbf33bf0464f7cd099f3f4c
Richard-AC <richard_abou_chaaya@brown.edu>
2021-11-23 09:10:27 -0500

    Adds caching to the PPRF-GGM computation

---
+++ source/PPRF_opt_server.py
@@ -0,0 +1,43 @@
+import pickle
+import socketserver
+import time
+
+import numpy as np
+
+from PPRF_GGM import *
+
+
+class PPRFOptServer(socketserver.BaseRequestHandler):
+
+    def handle(self):
+
+        BUFF_SIZE = 4096  # 4 KiB
+        data = b''
+        while True:
+            part = self.request.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                break
+
+        # print(len(data))
+
+        pk = pickle.loads(data)  # punctured key
+        print("Received vector from client")
+
+        print("Calculating GGM")
+        t1 = time.time()
+        keyword_vector = np.zeros((self.server.nwords))
+        for idx in range(self.server.nwords):
+            if idx % 1000 == 0:
+                print("Currently computing PPRF(K, {})".format(idx))
+            keyword_vector[idx] = Eval_opt(pk, idx)
+
+        print("Calculating scores for vector")
+
+        scores = self.server.tfidf.dot(keyword_vector)
+        t2 = time.time()
+
+        print(f"Calculated GGM and scores in {t2 - t1} seconds")
+        # print(len(pickle.dumps(scores)))
+
+        self.request.sendall(pickle.dumps(scores))

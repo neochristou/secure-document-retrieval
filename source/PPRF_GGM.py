0dc2681a8996f528dfee228792f9419fe6c4c2c0
Richard-AC <richard_abou_chaaya@brown.edu>
2021-11-23 09:08:37 -0500

    Adds caching to the PPRF-GGM computation

--- source/PPRF_GGM.py
+++ source/PPRF_GGM.py
@@ -2,6 +2,7 @@ import random
 
 # nbits = log2(nbr of keywords)
 # nwords = 474808
+#nbits = 3  # The height of the tree
 nbits = 19  # The height of the tree
 
 # Size of the PRF values (in bits)
@@ -80,7 +81,7 @@ def Eval(punctured_key, idx):
         # print("Left to eval : {}".format(left))
         if (node == n):
             is_punctured_point = False
-            # print("Anchor point found at level {}".format(nbits - i))
+            # print("Anchor point found at level {}".format(nbits - i - 1))
             curr = n_value
             for b in left:
                 G0, G1 = prng(curr)
@@ -96,11 +97,57 @@ def Eval(punctured_key, idx):
         return v
 
 
+cache = [[-1 for j in range(2**(i+1))] for i in range(nbits)]
+
+def Eval_opt(punctured_key, idx):
+    # print("Requesting idx : {} from punctured key".format(idx))
+    is_punctured_point = True
+    for i in range(nbits):
+        bits = bin(idx)[2:].zfill(nbits)
+        node = bits[:nbits - i]
+        left = bits[nbits-i:]
+        level = nbits - i - 1
+        (n, n_value) = punctured_key[level]
+        #print("Current level : {}. Current value : {}. Known value at that level : {}".format(nbits-i-1, node, n))
+        # print("Left to eval : {}".format(left))
+        if (node == n) or cache[level][int(node, 2)] != -1:
+            current_node = node
+            if (node == n):
+                curr = n_value
+            if (node == n) or cache[level][int(node, 2)] != -1:
+                #print("Cache hit")
+                curr = cache[level][int(current_node, 2)]
+            is_punctured_point = False
+            # print("Anchor point found at level {}".format(nbits - i - 1))
+            level += 1
+            for b in left:
+                G0, G1 = prng(curr)
+                if b == '0':
+                    curr = G0
+                elif b == '1':
+                    curr = G1
+                current_node = current_node + b
+                cache[level][int(current_node, 2)] = curr
+                level += 1
+            return curr
+    if is_punctured_point:
+        x, v = punctured_key[-1]
+        assert(idx == x)
+        # print("Punctured point requested")
+        return v
+
 if __name__ == "__main__":
     KEY = 10
 
     pk = puncture(KEY, 3, 999)
 
+    print("Direct eval : {}. Eval with pk : {}".format(GMM(KEY, 5), Eval_opt(pk, 5)))
+    print("Direct eval : {}. Eval_opt with pk : {}".format(GMM(KEY, 6), Eval_opt(pk, 6)))
+    print("Direct eval : {}. Eval_opt with pk : {}".format(GMM(KEY, 7), Eval_opt(pk, 7)))
+    print("Direct eval : {}. Eval_opt with pk : {}".format(
+        GMM(KEY, 85), Eval_opt(pk, 85)))
+    print("Direct eval : {}. Eval_opt with pk : {}".format(GMM(KEY, 3), Eval_opt(pk, 3)))
+    exit()
     print("Direct eval : {}. Eval with pk : {}".format(GMM(KEY, 5), Eval(pk, 5)))
     print("Direct eval : {}. Eval with pk : {}".format(GMM(KEY, 6), Eval(pk, 6)))
     print("Direct eval : {}. Eval with pk : {}".format(GMM(KEY, 7), Eval(pk, 7)))
--- source/server.py
+++ source/server.py
@@ -4,6 +4,7 @@ import socketserver
 
 import config
 from PPRF_server import PPRFServer
+from PPRF_opt_server import PPRFOptServer
 from random_vecs_server import RandomVectorsServer
 from rv_opt_server import RandomVectorsOptServer
 
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(
         description="Choose secure matrix multiplication scheme")
     arg_parser.add_argument("--scheme",
-                            choices=["random-vectors", "rv-opt", "PPRF"],
+                            choices=["random-vectors", "rv-opt", "PPRF", "PPRF-opt"],
                             required=True,
                             help="Scheme to be used for matrix multiplication")
     arg_parser.add_argument("-p", "--port",
@@ -34,6 +35,8 @@ if __name__ == "__main__":
         server = RandomVectorsOptServer
     if args.scheme == "PPRF":
         server = PPRFServer
+    if args.scheme == "PPRF-opt":
+        server = PPRFOptServer
 
     with socketserver.TCPServer((config.HOST, args.port), server) as doc_server:
         doc_server.tfidf = tfidf
